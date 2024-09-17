1da553f37b4fb068e8479638e4607e73c56423cb
ksaru <ksarun.sait@icloud.com>
2024-09-16 22:56:12 -0400

    week3

---
+++ lab_task_2.py
@@ -0,0 +1,26 @@
+# Declare variables
+age = 26
+price = 15
+name = "KAL"
+is_student = True
+
+# Perform arithmetic operations
+sum_value = age + 15
+product_value = price 
+
+# Print results
+print("Age:", age)
+print("Price:", price)
+print("Name:", name)
+print("Is student:", is_student)
+print("Sum value:", sum_value)
+print("Product value:", product_value)
+
+# Ask the user to input a number
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(number, "is even")
+else:
+    print(number, "is odd")
